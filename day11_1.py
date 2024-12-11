7d3a522111c06d40e70bbbb88eea3a6b77a1e808
Willy Jacquet <willy.jacquet@thermofisher.com>
2024-12-11 15:38:36 +0100

    Day 11

---
+++ day11_1.py
@@ -0,0 +1,38 @@
+import json
+
+input = ''
+
+with open('inputs/day11.txt') as f:
+    input = f.read()
+
+input = [int(x) for x in input.split(' ')]
+
+
+def flat(arr):
+    return [e for l in arr for e in l]
+
+
+def apply(x):
+    if x == '*':
+        return []
+    if x == 0:
+        return [1]
+    xstr = str(x)
+    xlen = len(xstr)
+    if xlen % 2 == 0:
+        return [int(xstr[:xlen//2]), int(xstr[xlen//2:])]
+    return [x * 2024]
+
+
+def apply_n(x, n):
+    arr = [x]
+    for i in range(n):
+        arr = flat([apply(x) for x in arr])
+    return arr
+
+total = 0
+
+for x in input:
+    total += len(apply_n(x, 25))
+
+print(total)
---
+++ day11_2.py
@@ -0,0 +1,57 @@
+import time
+
+input = ''
+
+with open('inputs/day11.txt') as f:
+    input = f.read()
+
+input = [int(x) for x in input.split(' ')]
+
+
+def flat(arr):
+    return [e for l in arr for e in l]
+
+
+def apply(x):
+    if x == 0:
+        return [1]
+    xstr = str(x)
+    xlen = len(xstr)
+    if xlen % 2 == 0:
+        return [int(xstr[:xlen // 2]), int(xstr[xlen // 2:])]
+    return [x * 2024]
+
+
+def apply_n(x, n):
+    arr = [x]
+    for i in range(n):
+        arr = flat([apply(x) for x in arr])
+    return arr
+
+
+cache = {}
+
+
+def count_n(x, n, step):
+    cache.setdefault(n, {})
+    if n < step:
+        return len(apply_n(x, n))
+    if x in cache[n]:
+        return cache[n][x]
+    cnt = sum([count_n(y, n - step, step) for y in apply_n(x, step)])
+    cache[n][x] = cnt
+    return cnt
+
+
+tot = 0
+
+start_time = time.time()
+
+for x in input:
+    tot += count_n(x, 75, 3)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print("Elapsed time:", elapsed_time, "seconds")
+
+print(tot)
