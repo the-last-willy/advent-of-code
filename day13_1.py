ba913f286ecd427bd44fc5f27d073691a45ab3b4
Willy Jacquet <willy.jacquet@thermofisher.com>
2024-12-16 09:10:35 +0100

    Day 13

---
+++ day13_1.py
@@ -0,0 +1,36 @@
+import re
+
+input = ''
+
+with open('inputs/day13.txt', 'r') as f:
+    input = f.read()
+
+numbers = [int(x) for x in re.findall(r'\d+', input)]
+
+
+def combinations(a, b, s):
+    res = []
+    for i in range(100):
+        left = i * a
+        if left > s:
+            continue
+        if (s - left) % b == 0:
+            j = (s - left) // b
+            res += [(i, j)]
+    return res
+
+credits = 0
+
+for i in range(0, len(numbers), 6):
+    ax, ay, bx, by, px, py = numbers[i:i+6]
+    acs = set(combinations(ax, bx, px))
+    bcs = set(combinations(ay, by, py))
+    possibilities = acs & bcs
+    print(possibilities)
+    if len(possibilities) > 1:
+        raise Exception('Multiple possibilities')
+    elif len(possibilities) == 1:
+        p = list(possibilities)[0]
+        credits += 3 * p[0] + p[1]
+
+print(credits)
---
+++ day13_2.py
@@ -0,0 +1,22 @@
+import re
+
+input = ''
+
+with open('inputs/day13.txt', 'r') as f:
+    input = f.read()
+
+numbers = [int(x) for x in re.findall(r'\d+', input)]
+
+credits = 0
+
+for i in range(0, len(numbers), 6):
+    ax, ay, bx, by, px, py = numbers[i:i+6]
+    px += 10000000000000
+    py += 10000000000000
+    af = (by * px - bx * py) / (by * ax - bx * ay)
+    bf = (py - ay * af) / by
+
+    if af % 1 == 0 and bf % 1 == 0:
+        credits += 3 * af + bf
+
+print(credits)
